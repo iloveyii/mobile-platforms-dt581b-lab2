ec367daddb2997f265fa09b5031d166c9e54f438
Hazrat Ali <iloveyii@yahoo.com>
2020-09-06 20:41:57 +0200

    Added Drawer

--- react/src/Layouts/Header.js
+++ react/src/Layouts/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {AppBar, Toolbar, IconButton, Typography, Button, makeStyles} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from "../Components/Drawer";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -18,17 +19,34 @@ const useStyles = makeStyles((theme) => ({
 export default props => {
     const classes = useStyles();
 
+    const [state, setState] = React.useState({
+        open: false,
+    });
+
+    const toggleDrawer = (open) => (event) => {
+        console.log('toggleDrawer ', open)
+        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+        setState({open});
+    };
+
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon/>
-                </IconButton>
-                <Typography variant="h6" className={classes.title}>
-                    News
-                </Typography>
-                <Button color="inherit">Login</Button>
-            </Toolbar>
-        </AppBar>
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton onClick={toggleDrawer(true)} edge="start" className={classes.menuButton} color="inherit"
+                                aria-label="menu">
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+
+            <Drawer toggleDrawer={toggleDrawer} state={state}/>
+        </>
     )
 }
