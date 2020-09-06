9fcabbc6dbaa767cf3d1f1e596e1c0cde54151a6
Hazrat Ali <iloveyii@yahoo.com>
2020-09-06 13:13:31 +0200

    Fixed footer

--- react/src/Layouts/Footer.js
+++ react/src/Layouts/Footer.js
@@ -3,35 +3,42 @@ import {Paper, Tabs, Tab} from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import PhoneIcon from '@material-ui/icons/Phone';
 import PersonPinIcon from '@material-ui/icons/PersonPin';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
+import Box from "@material-ui/core/Box";
 
 
 const useStyles = makeStyles({
     root: {
-        flexGrow: 1,
-        maxWidth: '100%',
         position: 'fixed',
-        bottom: 0
+        bottom: 0,
+        width: '100%'
+    },
+    paper: {
+        flexGrow: 1,
     },
 });
 
 export default props => {
     const classes = useStyles();
 
-    return(
-        <Paper square className={classes.root}>
-            <Tabs
-                value={0}
-                onChange={()=>null}
-                variant="fullWidth"
-                indicatorColor="primary"
-                textColor="primary"
-                aria-label="icon tabs example"
-            >
-                <Tab icon={<PhoneIcon />} aria-label="phone" />
-                <Tab icon={<FavoriteIcon />} aria-label="favorite" />
-                <Tab icon={<PersonPinIcon />} aria-label="person" />
-            </Tabs>
-        </Paper>
+    return (
+        <Box className={classes.root}>
+
+            <Paper square className={classes.paper}>
+                <Tabs
+                    value={0}
+                    onChange={() => null}
+                    variant="fullWidth"
+                    indicatorColor="primary"
+                    textColor="primary"
+                    aria-label="icon tabs example"
+                    centered
+                >
+                    <Tab icon={<PhoneIcon/>} aria-label="phone"/>
+                    <Tab icon={<FavoriteIcon/>} aria-label="favorite"/>
+                    <Tab icon={<PersonPinIcon/>} aria-label="person"/>
+                </Tabs>
+            </Paper>
+        </Box>
     )
 }
