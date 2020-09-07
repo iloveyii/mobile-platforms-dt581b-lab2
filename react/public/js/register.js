06ceb549e5eeeb2ea82193a774e3c4ef56b9512e
Hazrat Ali <iloveyii@yahoo.com>
2020-09-07 12:42:00 +0200

    Added register.js

---
+++ react/public/js/register.js
@@ -0,0 +1,29 @@
+async function setCurrentVersion() {
+    console.log('Service worker fetch version')
+    const response = await fetch('/version.json');
+    const data = await response.json();
+    localStorage.setItem('sw_version', data.version);
+    await clearOldCaches(data.version);
+}
+
+// Remove all older than version, To delete set version > 1 in version.json
+async function clearOldCaches(version) {
+    const keys = await caches.keys();
+    keys.map(async (k) => {
+        if (!k.includes('-v' + version)) {
+            console.log('Service worker deleting version', k);
+            await caches.delete(k);
+        }
+    });
+}
+
+if (false && 'serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(async function (event) {
+            console.log('Service worker registered', event.scope);
+            await setCurrentVersion();
+        }).catch(function (error) {
+        console.log('Error in SW : ', error);
+    })
+}
